315f6656c46af8d5a9a56246c3c273b7f56df939
Gonzalo <agustinlamas2011@gmail.com>
2025-09-06 02:20:03 -0300

    Cambios el 06/08

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tienda'
+    'tienda',
+    'rest_framework',
+    'usuarios',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +77,15 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce_db',     # nombre de tu BD
+        'USER': 'postgres',         # usuario de postgres
+        'PASSWORD': '1234',  # contraseña de postgres
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
