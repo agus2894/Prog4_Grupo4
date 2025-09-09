09b2f6bd03d587d94d5c3179ab1d843b38964c3f
Ian Medina <ianchuloooo@gmail.com>
2025-09-08 22:20:05 -0300

    Agregado registro de usuarios

--- tienda/urls.py
+++ tienda/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout_view, name='logout'),
+    path('shop/', views.shop, name='shop'),  
 ]
