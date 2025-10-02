66c3eb6dd23b9eb392bd2b76655185974a88b819
Gonzalo <agustinlamas2011@gmail.com>
2025-10-02 19:30:10 -0300

    Cambios 02/10

--- usuarios/models.py
+++ usuarios/models.py
@@ -2,6 +2,11 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import URLValidator
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
